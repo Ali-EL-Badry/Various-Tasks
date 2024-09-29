6ca8da376913d61fc4c7fd8bb726dd106856b1ec
AiBaK <11410120231109@stud.cu.edu.eg>
2024-09-29 22:38:33 +0300

    Update main.cpp

--- Task 1/main.cpp
+++ Task 1/main.cpp
@@ -2,7 +2,7 @@
  * File Name : Task 1 (Tempo)
  * Purpose : (Tempo)
  * Section : (Tempo)
- * Auther 1 of (1|4|7|10) : Alaa Tarik                            * ID 1 :
+ * Auther 1 of (1|4|7|10) : Alaa Tarik Mohammed Salah El-Deen     * ID 1 : 20230064
  * Auther 2 of (2|5|8|11) : Fatema El-Zhraa Ahmed Mohamed El-Fiky * ID 2 : 20230280
  * Auther 3 of (3|6|9|12) : Aly El-Deen Yasser Ali Mohammed       * ID 3 : 20231109
  * TA :
@@ -15,11 +15,23 @@
 using namespace std;
 
 // Common Used Functions :
-
+string check_menu(const string& menuText , vector<string>choices){
+    string correct;
+    while(true){
+        cout << menuText ;
+        getline(cin, correct);
+        if(correct.size() != 1 && find(choices.begin(),choices.end(), correct) != choices.end())
+            cout << "Please Enter a valid option\n" ;
+        else
+            break;
+        cin.ignore();
+    }
+    return correct;
+}
 
 // Problem 1 :
 
-// Problem 2 : 
+// Problem 2 :
 
 // Problem 3 :
 

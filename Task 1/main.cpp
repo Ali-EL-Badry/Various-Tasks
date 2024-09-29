e7f9543fa2e91aff44fea60738b0016a8e4fe031
AiBaK <11410120231109@stud.cu.edu.eg>
2024-09-29 22:18:45 +0300

    Update main.cpp

--- Task 1/main.cpp
+++ Task 1/main.cpp
@@ -1,8 +1,49 @@
+/*
+ * File Name : Task 1 (Tempo)
+ * Purpose : (Tempo)
+ * Section : (Tempo)
+ * Auther 1 of (1|4|7|10) : Alaa Tarik                            * ID 1 :
+ * Auther 2 of (2|5|8|11) : Fatema El-Zhraa Ahmed Mohamed El-Fiky * ID 2 : 20230280
+ * Auther 3 of (3|6|9|12) : Aly El-Deen Yasser Ali Mohammed       * ID 3 : 20231109
+ * TA :
+ * Date : 8 Oct 2023
+ */
+
+// Defining only
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
 
-int main(){
+// Common Used Functions :
 
+
+// Problem 1 :
+
+// Problem 2 : 
+
+// Problem 3 :
+
+// Problem 4 :
+
+// Problem 5 :
+
+// Problem 6 :
+
+// Problem 7 :
+
+// Problem 8 :
+
+// Problem 9 :
+
+// Problem 10:
+
+// Problem 11:
+
+// Problem 12:
+
+// Menu :
+
+
+int main(){
     return 0;
 }

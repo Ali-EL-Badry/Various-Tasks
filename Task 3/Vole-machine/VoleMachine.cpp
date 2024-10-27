e4447f285d6fe3bff7cc3cdd1e11ec5d68e13f30
Fatema ELzhraa Elfiky <143023056+fatema123elfiky@users.noreply.github.com>
2024-10-27 23:57:10 +0300

    Some changes in machine and cpu

--- Task 3/Vole-machine/VoleMachine.cpp
+++ Task 3/Vole-machine/VoleMachine.cpp
@@ -1,3 +1,27 @@
 #include <iostream>
 #include "VoleMachine.h"
 #include <vector>
+
+//**
+void CPU::clear() {
+    IR.clear();
+    PC->clear();
+    //clean registers
+}
+//**
+void Machine ::clear( string option) {
+
+    if(option=="1"){
+
+        cpu.clear();
+    }else if( option=="2"){
+
+        //clean memory
+
+    }else{
+
+        cpu.clear();
+        //clean memory
+    }
+
+}
--- Task 3/Vole-machine/VoleMachine.h
+++ Task 3/Vole-machine/VoleMachine.h
@@ -1,18 +1,62 @@
 #ifndef VOLE_MACHINE_VOLEMACHINE_H
 #define VOLE_MACHINE_VOLEMACHINE_H
+#include <iostream>
 
+using namespace std;
+
+//Classes
 
 class Memory{};
 class Register{};
 class CU{};
 class ALU{};
-class CPU{};
+
+// the required elements from the cpu --> IR, PC ,clear cpu
+class CPU{
+private:
+
+    string IR ;
+    string * PC ;
+    Register registers;
+    ALU alu;
+    CU cu ;
+
+    void valid(string IR );
+
+public:
+    // show the PC ,IR after we finish
+    void fetch(string * PC );
+
+    //explain the decode or the meaning od the instruction means what by the cout
+    // 1 -> cu // 2->alu // 3->c000
+    int decode(string IR);
+
+    //execute then show memo, registers
+    void excute(int Case);
+
+    void clear();//++
+    string getter_PC();
+    string getter_IR();
+    void show_cpu();
+
+    //void setter_register(location , value) ;
+
+
+
+};
 class Machine {
 private:
   CPU cpu;
   Memory memory;
 
 public:
+
+    // show options to clear //++
+    void clear(string option );// 1. if to clear registers (of course will be the cpu too),2. clean memo ,3. clean all
+
+    // Take option to see option of storing way
+    void load_file(bool option,string name);
+    void show_machine();
 
 };
 class UI{};

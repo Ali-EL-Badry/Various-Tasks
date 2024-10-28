6211ea5dde43b30d46b3b5153e9786980cccb0a0
Alaataarek <161245884+Alaataarek@users.noreply.github.com>
2024-10-28 16:06:58 +0300

    Update VoleMachine.cpp =>added the functions of Register and memory

--- Task 3/Vole-machine/VoleMachine.cpp
+++ Task 3/Vole-machine/VoleMachine.cpp
@@ -25,3 +25,103 @@ void Machine ::clear( string option) {
     }
 
 }
+// functions of Memory class
+//funcion to set an index
+void Memory::set_nstrctions(int row,int coloumn ,string &value)
+    {
+
+
+        while(cin.fail()||row>=16||row<0)
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cin>>row;
+        }
+        while(cin.fail()||coloumn>=16||coloumn<0)
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cin>>coloumn;
+        }
+        nstrctions[row][coloumn]=value;
+    }
+
+    void Memory:: clear()
+    {
+        for (auto& row : nstrctions)
+        {
+            std::fill(row.begin(), row.end(), "00");
+        }
+    }
+
+    string Memory::get_index(int row ,int coloumn)
+    {
+        while(cin.fail()||row>=16||row<0)
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cin>>row;
+        }
+        while(cin.fail()||coloumn>=16||coloumn<0)
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cin>>coloumn;
+        }
+        return nstrctions[row][coloumn];
+    }
+
+    void Memory:: print(){
+        for (int i = 0; i < 16; ++i) {
+            for (int j = 0; j <16 ; ++j) {
+                cout<<nstrctions[i][j];
+            }
+        }
+
+    }
+
+vector<vector<string>> Memory::get_nstrctions(){
+
+    return nstrctions;
+}
+//end of the functions of class Memory
+
+
+//functions of class Register
+
+  void  Register:: set_rgstr(int location,string &value)
+    {
+        while(cin.fail()||location>=16||location<0)
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cin>>location;
+        }
+        rgstr[location]=value;
+    }
+void Register:: clear()
+    {
+        rgstr.clear();
+    }
+
+
+string Register:: get_rgstr(int location)
+    {
+        while(cin.fail()||location>=16||location<0)
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cin>>location;
+        }
+    return rgstr[location];
+    }
+
+
+void Register ::print_rgstr()
+    {
+    for (int i = 0; i < 16; ++i)
+      {
+        cout<<rgstr[i]<<endl;
+      }
+    }
+//end of functions of class Register

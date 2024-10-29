be148a27ba46670518d1fd5b262fd7ebf8646605
Aly El-Deen Yasser <11410120231109@stud.cu.edu.eg>
2024-10-29 21:09:24 +0300

    Update Alu.h

--- Task 3/Machine/CPU/Alu and Cu/Alu/Alu.h
+++ Task 3/Machine/CPU/Alu and Cu/Alu/Alu.h
@@ -1,17 +1,56 @@
-
 #ifndef ALU_H
 #define ALU_H
 
 #include <string>
-#include <cmath>
+#include "../../Register/Register.h"
 using namespace std;
 
 class Alu {
-
+ 
 public:
+    /**
+     * @brief it change the decimal number to hexadecimal numbers.
+     *
+     * @param number the number in its decimal stat.
+     *
+     * @return  number in the hexadecimal stat.
+     */
     static string decToHex(const string& number);
+
+    /**
+     * @brief it change the hexadecimal number to decimal numbers.
+     *
+     * @param number the number in its Hexadecimal stat.
+     *
+     * @return  number in the Decimal stat.
+     */
     static string hexToDec(const string& number);
-    static void add(int firstIndex, int secondIndex);
+
+    /**
+     * @brief add the bit patterns in registers S and T as though they were two’s complement representations
+     *        and leave the result in register R.
+     *
+     * @param reg The register that the addition will happen and stored in
+     * @param firstRegIndex the index of the Register S
+     * @param secondRegIndex the index of the Register T
+     * @param thirdRegIndex the index of the Register R
+     *
+     */
+    static void addTwosComplement(Register &reg, const char &firstRegIndex, const char &secondRegIndex, const char &thirdRegIndex);
+
+    /**
+    * @brief add the bit patterns in registers S and T as thoughthey represented values in floating-point notation
+    *        and leave the result in register R.
+    *
+    * @param reg The register that the addition will happen and stored in
+    * @param firstRegIndex the index of the Register S
+    * @param secondRegIndex the index of the Register T
+    * @param thirdRegIndex the index of the Register R
+    *
+    */
+    static void addFloatingPoint(Register &reg, const char &firstRegIndex, const char &secondRegIndex, const char &thirdRegIndex);
+
+
 };
 
 

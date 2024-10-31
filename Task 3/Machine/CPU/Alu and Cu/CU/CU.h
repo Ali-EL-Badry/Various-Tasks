d4ed893a39b875c60cc9597768dabf3e1fe616fc
Aly El-Deen Yasser <11410120231109@stud.cu.edu.eg>
2024-10-31 16:27:13 +0300

    Update CU.h

--- Task 3/Machine/CPU/Alu and Cu/CU/CU.h
+++ Task 3/Machine/CPU/Alu and Cu/CU/CU.h
@@ -11,19 +11,19 @@ class CU {
 private:
     /**
      * @brief  Convert hexadecimal number to decimal number
-     *
+     * 
      * @param  number The hexadecimal number
-     *
+     * 
      * @return The decimal number after being converted
      */
-     string hexToDec(const string& number);
+    static int hexToDec(const char& number);
 
 public:
     /**
      * @brief load the register R with the bit pattern found in the memory cell whose address is XY.
-     *
+     * 
      * @param addressRegister The address of the register.
-     * @param addressmemory The address of the memory.
+     * @param addressMemory The address of the memory.
      * @param memory  The memory which contain the value to be loaded.
      * @param reg The register where the content of the memory will be stored.
      */
@@ -31,16 +31,16 @@ public:
 
     /**
      * @brief load the register R with the bit pattern found in the memory cell whose address is XY.
-     *
+     * 
      * @param reg The register where the value will be loaded.
-     * @param adress The address of the register.
+     * @param address The address of the register.
      * @param value The value to be loaded.
      */
-    static void loadwith(Register& reg, const char &adress, string &value);
+    static void loadwith(Register& reg, const char &address, string &value);
 
     /**
      * @brief Store the content of register R in the memory cell whose address is XY.
-     *
+     * 
      * @param memory The memory where the content of register will be stored.
      * @param reg The register which contain the value.
      * @param addressRegister The address of the register.
@@ -51,19 +51,18 @@ public:
     /**
      * @brief  Stores the content of the register R in the address 00 which is the screen.
      *
-     * @param memory The memory where the content of the screen register will be stored.
+     * @param reg The register where the content will be stored to the screen.
      * @param addressRegister The address of the register containing the address of the memory location.
-     * @param addressMemory The address of the memory location where the content of the screen register will be stored.
      */
-    static void storeScreen(Memory& memory, const char &addressRegister, const string& addressMemory);
-
+    static void storeScreen(Register &reg , const char &addressRegister);
+    
     /**
      * @brief  Move the bits of the register R to register S.
      *
      * @param reg The register where the two register belong to.
      * @param firstRegisterAddress The address of the first register.
      * @param secondRegisterAddress The address of the second register
-     *
+     * 
      */
     static void moveBits(Register &reg, const char &firstRegisterAddress, const char &secondRegisterAddress );
 
@@ -71,12 +70,23 @@ public:
      * @brief  Jump to the memory location whose address is XY if value of register in address R equals to register in address 0.
      *
      * @param reg The register to check that value of register in address R equals to register in address 0.
-     * @param memory The memory where the operation will be done.
-     * @param addressMemory The address of the memory location where the program will jump to.
-     * @param RegisterAddress The address of the register R.
-     *
+     * @param Address The address of the register R.
+     * @param currentPC The place where the Program counter is.
+     * @param row the index of the row in the memory cells
+     * @param column the index of the colmn in the memory cells
      */
-    static void jumpToLocation(Register &reg, Memory &memory, const string &addressMemory, const char &RegisterAddress);
+    static void jumpToEqualLocation(Register &reg, const string &Address, string& currentPC, int &row , int &column);
+
+    /**
+     * @brief  Jump to the memory location whose address is XY if value of register in address R greater than register in address 0.
+     *
+     * @param reg The register to check that value of register in address R greater than register in address 0.
+     * @param Address The address of the register R.
+     * @param currentPC The place where the Program counter is.
+     * @param row the index of the row in the memory cells
+     * @param column the index of the colmn in the memory cells
+     */
+    static void jumpToSmallerLocation(Register &reg, const string &Address, string& currentPC, int &row , int &column);
 };
 
 #endif
